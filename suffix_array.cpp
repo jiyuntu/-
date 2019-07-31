ce99a1156e765e84b4b9925da3eed6c1e1f92296
jiyuntu <53334017+jiyuntu@users.noreply.github.com>
2019-07-31 15:24:56 +0800

    Update suffix_array.cpp

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -9,17 +9,17 @@ int sa[maxl],c[maxl],x[maxl],y[maxl];
 
 void build_suffix_array(int m){
     int l=strlen(s);
-    for(int i=0;i<maxm;i++) c[i]=0;
+    for(int i=0;i<m;i++) c[i]=0;
     for(int i=0;i<l;i++) c[x[i]=s[i]-'0']++;
-    for(int i=1;i<maxm;i++) c[i]+=c[i-1];
+    for(int i=1;i<m;i++) c[i]+=c[i-1];
     for(int i=l-1;i>=0;i--) sa[--c[x[i]]]=i;
     for(int k=1;k<=l;k<<=1){
         int p=0;
         for(int i=l-k;i<l;i++) y[p++]=i;
         for(int i=0;i<l;i++) if(sa[i]>=k) y[p++]=sa[i]-k;
-        for(int i=0;i<maxm;i++) c[i]=0;
+        for(int i=0;i<m;i++) c[i]=0;
         for(int i=0;i<l;i++) c[x[y[i]]]++;
-        for(int i=1;i<maxm;i++) c[i]+=c[i-1];
+        for(int i=1;i<m;i++) c[i]+=c[i-1];
         for(int i=l-1;i>=0;i--) sa[--c[x[y[i]]]]=y[i];
         swap(x,y);//y of no use
         p=1; x[sa[0]]=0;
